962bebdd1138c8267d3904459f298f198cd1f68b
LuisPa <diganluispa@gmail.com>
2018-11-13 11:52:14 -0600

    fc

---
+++ backend/handler.js
@@ -0,0 +1,34 @@
+'use strict';
+var loremIpsum = require('lorem-ipsum'),
+	output = loremIpsum();
+
+function generate() {
+	var output = loremIpsum({
+		count: 10, // Number of words, sentences, or paragraphs to generate.
+		units: 'paragraphs', // Generate words, sentences, or paragraphs.
+		sentenceLowerBound: 5, // Minimum words per sentence.
+		sentenceUpperBound: 15, // Maximum words per sentence.
+		paragraphLowerBound: 3, // Minimum sentences per paragraph.
+		paragraphUpperBound: 7, // Maximum sentences per paragraph.
+		format: 'plain', // Plain text or html
+		//, words: ['ad', 'dolor', ... ]  // Custom word dictionary. Uses dictionary.words (in lib/dictionary.js) by default.
+		random: Math.random // A PRNG function. Uses Math.random by default
+	});
+	return output;
+}
+
+function createSuccessResponse(response) {
+	return {
+		statusCode: 200,
+		headers: {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Credentials': true,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(response)
+	};
+}
+
+module.exports.fun = (event, context, callback) => {
+	callback(null, createSuccessResponse(generate()));
+};
---
+++ frontend/README.md
@@ -0,0 +1,18 @@
+# Lorem ipsum Generator for #NoServerNovember
+
+## Demo: https://lorem-ipsum-serverless.netlify.com
+
+To deploy it:
+
+### Frontend (Using zeit.sh)
+
+```bash
+$ now
+```
+
+### Backend (Using serverless framework + AWS)
+
+```bash
+$ yarn && sls deploy
+```
+
